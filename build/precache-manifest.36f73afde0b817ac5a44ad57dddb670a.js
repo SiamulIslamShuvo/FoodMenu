5dc7daf06fe8dbcb4b2e636e9b95514a7c78f953
SiamulIslamShuvo <54581583+SiamulIslamShuvo@users.noreply.github.com>
2021-02-09 21:26:45 +0600

    updated

---
+++ build/precache-manifest.36f73afde0b817ac5a44ad57dddb670a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fcae6d1b0bf7aabc88c2c414e19467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2758584116ac1c1f7655",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0aa31a77513f04eafea2",
+    "url": "/static/js/2.d1ec9115.chunk.js"
+  },
+  {
+    "revision": "2758584116ac1c1f7655",
+    "url": "/static/js/main.b95ba25b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
